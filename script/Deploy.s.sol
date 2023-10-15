bb0caee659107b87ceb35dcc16190f0120f200cd
Nick Furfaro <nfurfaro33@gmail.com>
2023-10-15 17:41:00 -0600

    test out change in deployment

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,13 +4,26 @@ pragma solidity ^0.8.15;
 import "foundry-huff/HuffDeployer.sol";
 import "forge-std/Script.sol";
 
-interface SimpleStore {
-  function setValue(uint256) external;
-  function getValue() external returns (uint256);
+interface Huffbits {
+  function bitMask(uint256) external pure returns(uint256);
+  function nibbleMask(uint256) external pure returns(uint256);
+  function byteMask(uint256) external pure returns(uint256);
+  function multiMask(uint256) external pure returns(uint256);
+  function setBit(uint256, uint256) external pure returns(uint256);
+  function clearBit(uint256, uint256) external pure returns(uint256);
+  function toggleBit(uint256, uint256) external pure returns(uint256);
+  function queryBit(uint256, uint256) external pure returns(uint256);
+  function writeNibble(uint256, uint256, uint256) external pure returns(uint256);
+  function clearNibble(uint256, uint256) external pure returns(uint256);
+  function queryNibble(uint256, uint256) external pure returns(uint256);
+  function clearByte(uint256, uint256) external pure returns(uint256);
+  function writeByte(uint256, uint256, uint256) external pure returns(uint256);
+  function queryByte(uint256, uint256) external pure returns(uint256);
 }
 
 contract Deploy is Script {
-  function run() public returns (SimpleStore simpleStore) {
-    simpleStore = SimpleStore(HuffDeployer.deploy("SimpleStore"));
+  function run() public returns (Huffbits huffbits) {
+    string memory wrappers = vm.readFile("test/mocks/HuffbitsWrappers.huff");
+    huffbits = Huffbits(HuffDeployer.deploy_with_code("Huffbits", wrappers));
   }
 }
